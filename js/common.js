6a7a89d5eb4e1a31526bd767ed7ff265339d1ebb
artyom-lab <artyomgalkeane@gmail.com>
2020-05-18 19:12:03 +0300

    67

---
+++ js/common.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+  function headerSticky() {
+    if ($(window).scrollTop() > 0) {
+      $('.navbar.fixed-top').addClass("sticky");
+    } else {
+      $('.navbar.fixed-top').removeClass("sticky");
+    }
+  };
+
+  $(window).scroll(function() {
+    headerSticky();
+  });
+  headerSticky();
+
+  Waves.attach('.btn', ['waves-light']);
+  Waves.init();
+
+  $('#collapse-1').on('click', function() {
+    $('#collapse-1-1').toggleClass('active');
+  });
+
+  $(document).on('click.bs.dropdown.data-api', '.dropdown-menu', function (e) { 
+    e.stopPropagation();
+  });
+
+});
+
